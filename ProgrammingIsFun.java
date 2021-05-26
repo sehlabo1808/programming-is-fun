78d26521158067c3dd6f844752842340acfee7e1
Sehlabo1808 <84082493+sehlabo1808@users.noreply.github.com>
2021-05-26 15:01:47 +0200

    Add files via upload

---
+++ ProgrammingIsFun.java
@@ -0,0 +1,13 @@
+//Programmingisfun.java
+package com.gmail.temohosehlabo;
+import javax.swing.JOptionPane;
+
+public class ProgrammingIsFun {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Programming is fun!");
+		System.exit(0);
+
+	}
+
+}
